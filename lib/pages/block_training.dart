a82bb87b39f24104330dc87c77c41a1d99ae1f45
Viktor <rickeygame23@gmail.com>
2025-06-07 22:30:06 +0300

    e

---
+++ lib/pages/block_training.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Bloc_Training extends StatefulWidget {
+  const Bloc_Training({super.key});
+
+  @override
+  State<Bloc_Training> createState() => _Bloc_TrainingState();
+}
+
+class _Bloc_TrainingState extends State<Bloc_Training> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
